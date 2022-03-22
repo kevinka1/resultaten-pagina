23b775780e7b38286560d8d8e59c6c57ace3fbc8
Remon Dollenkamp <remon2004@outlook.com>
2022-03-22 11:33:00 +0100

    update

--- Meetmoment 1/Meetmoment 1/Form1.Designer.cs
+++ Meetmoment 1/Meetmoment 1/Form1.Designer.cs
@@ -29,13 +29,60 @@ namespace Meetmoment_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.vlak = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.vlak.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // vlak
+            // 
+            this.vlak.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.vlak.Controls.Add(this.button2);
+            this.vlak.Controls.Add(this.button1);
+            this.vlak.Location = new System.Drawing.Point(214, 96);
+            this.vlak.Name = "vlak";
+            this.vlak.Size = new System.Drawing.Size(350, 223);
+            this.vlak.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "verder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "terug";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vlak);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.vlak.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel vlak;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
