49d9d012ece508a60854bf05f4ea34c56987d95b
Remon Dollenkamp <remon2004@outlook.com>
2022-03-22 13:39:44 +0100

    s

--- Meetmoment 1/Meetmoment 1/Form1.Designer.cs
+++ Meetmoment 1/Meetmoment 1/Form1.Designer.cs
@@ -30,14 +30,21 @@ namespace Meetmoment_1
         private void InitializeComponent()
         {
             this.vlak = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.vlak.SuspendLayout();
             this.SuspendLayout();
             // 
             // vlak
             // 
-            this.vlak.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.vlak.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vlak.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.vlak.Controls.Add(this.comboBox1);
+            this.vlak.Controls.Add(this.label1);
             this.vlak.Controls.Add(this.button2);
             this.vlak.Controls.Add(this.button1);
             this.vlak.Location = new System.Drawing.Point(214, 96);
@@ -45,24 +52,51 @@ namespace Meetmoment_1
             this.vlak.Size = new System.Drawing.Size(350, 223);
             this.vlak.TabIndex = 0;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(253, 191);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "verder";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.Silver;
             this.button2.Location = new System.Drawing.Point(3, 191);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 2;
             this.button2.Text = "terug";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(253, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "verder";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "klantgericht";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "O",
+            "V",
+            "G"});
+            this.comboBox1.Location = new System.Drawing.Point(253, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "kies";
             // 
             // Form1
             // 
@@ -74,6 +108,7 @@ namespace Meetmoment_1
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.vlak.ResumeLayout(false);
+            this.vlak.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -83,6 +118,8 @@ namespace Meetmoment_1
         private System.Windows.Forms.Panel vlak;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
